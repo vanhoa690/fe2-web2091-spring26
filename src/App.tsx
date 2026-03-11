d6965aa4fb7a779e205ef204e60c8db6a6e61e74
vanhoa690 <vanhoa690@gmail.com>
2026-03-11 15:28:33 +0700

    Image antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
 import { Toaster } from "react-hot-toast";
 import { Link } from "react-router-dom";
-import { Form, Input, Button } from "antd";
+import { Form, Input, Button, Table } from "antd";
 import { Layout } from "antd";
-
+import { Image } from "antd";
 const { Header, Content, Footer } = Layout;
 
 function App() {
   const onFinish = (values: any) => {
     console.log(values);
   };
+
+  const columns = [
+    { title: "Họ tên", dataIndex: "name" },
+    { title: "Tuổi", dataIndex: "age" },
+    { title: "Môn học", dataIndex: "subject" },
+    {
+      title: "Avatart",
+      dataIndex: "avatar",
+      render: (avatar: string) => <Image width={50} src={avatar} />,
+    },
+  ];
+
+  const data = [
+    {
+      key: 1,
+      name: "John",
+      age: 25,
+      subject: "FE2",
+      avatar: "https://i.pravatar.cc/150?img=1",
+    },
+    {
+      key: 2,
+      name: "Anna",
+      age: 30,
+      subject: "FE1",
+      avatar: "https://i.pravatar.cc/150?img=2",
+    },
+  ];
+
   return (
     <>
       <nav className="bg-blue-600 text-white shadow">
@@ -44,10 +73,6 @@ function App() {
       <div className="max-w-6xl mx-auto mt-10 px-4 text-center">
         <h1 className="text-4xl font-bold mb-4">Chào mừng đến với WEB2091</h1>
         <Button type="primary">Click me</Button>
-        <Button type="default">Click me</Button>
-        <Button type="dashed">Click me</Button>
-        <Button type="link">Click me</Button>
-        <Button type="text">Click me</Button>
         <Layout>
           <Header style={{ color: "white" }}>Header</Header>
           <Content style={{ padding: 20 }}>
@@ -61,6 +86,7 @@ function App() {
                 </Button>
               </Form.Item>
             </Form>
+            <Table columns={columns} dataSource={data} />
           </Content>
           <Footer>Footer</Footer>
         </Layout>

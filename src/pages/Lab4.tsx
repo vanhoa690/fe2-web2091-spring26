7fb24b2aca8ca9faf71f000d207a420b4f12a782
vanhoa690 <vanhoa690@gmail.com>
2026-03-17 14:52:58 +0700

    useMatation

--- src/pages/Lab4.tsx
+++ src/pages/Lab4.tsx
@@ -1,15 +1,28 @@
+import { useMutation } from "@tanstack/react-query";
 import { Button, Form, Input } from "antd";
+import axios from "axios";
 
 export default function StoryForm() {
+  const { mutate, isPending, isSuccess } = useMutation({
+    mutationFn: async (values: any) => {
+      await axios.post("http://localhost:3000/stories", values);
+    },
+  });
+
   const onFinish = (values: any) => {
-    console.log("submit", values);
+    mutate(values);
   };
   return (
     <Form layout="vertical" onFinish={onFinish}>
       <Form.Item label="Title" name="title">
         <Input placeholder="title" />
       </Form.Item>
-      <Button htmlType="submit">Submit</Button>
+      <Button htmlType="submit" loading={isPending} type="primary">
+        Submit
+      </Button>
+      {isSuccess && (
+        <div style={{ color: "green" }}>Story created successfully!</div>
+      )}
     </Form>
   );
 }

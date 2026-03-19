cebd1be0841cff9d96fca31246411451b67183ae
vanhoa690 <vanhoa690@gmail.com>
2026-03-19 14:54:54 +0700

    delete useMuatation

--- src/pages/Lab5.tsx
+++ src/pages/Lab5.tsx
@@ -1,13 +1,21 @@
-import { useQuery } from "@tanstack/react-query";
-import { Image, Table } from "antd";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Button, Image, Popconfirm, Table } from "antd";
 import axios from "axios";
 
 export function StoryList() {
   const { data, isLoading } = useQuery({
-    queryKey: ["getAllStories"],
+    queryKey: ["stories"],
     queryFn: async () => {
       const res = await axios.get("http://localhost:3000/stories");
       return res.data;
+    },
+  });
+  const qc = useQueryClient();
+  const { mutate } = useMutation({
+    mutationFn: async (id: number) =>
+      await axios.delete(`http://localhost:3000/stories/${id}`),
+    onSuccess: () => {
+      qc.invalidateQueries({ queryKey: ["stories"] });
     },
   });
   const columns = [
@@ -23,6 +31,20 @@ export function StoryList() {
       title: "Hinh anh",
       dataIndex: "image",
       render: (src: string) => <Image src={src} height={100} />,
+    },
+    {
+      title: "Action",
+      render: (_: any, record: any) => (
+        <Popconfirm
+          title="Delete the story"
+          description="Are you sure to delete this story?"
+          okText="Yes"
+          cancelText="No"
+          onConfirm={() => mutate(record.id)}
+        >
+          <Button danger>Delete</Button>
+        </Popconfirm>
+      ),
     },
   ];
 

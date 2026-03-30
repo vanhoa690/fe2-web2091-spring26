9c242f37638961a696079a61b362574b88e17a48
vanhoa690 <vanhoa690@gmail.com>
2026-03-30 14:49:34 +0700

    boc provider cho app

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { UserProvider } from "./context/UserContext";
 
 const queryClient = new QueryClient();
 
@@ -11,7 +12,11 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <UserProvider>
+          {/* ThemeProvider  */}
+          <App />
+          {/* AuthProvider */}
+        </UserProvider>
       </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
